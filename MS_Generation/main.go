0ee4d7de064fe6ed879470741f082261693c61fc
iavealokin <red.joker.rus@gmail.com>
2021-01-26 16:46:58 +0500

    Add Proto

--- MS_Generation/main.go
+++ MS_Generation/main.go
@@ -1,15 +1,69 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"math/rand"
-	"strings"
+	"context"
+	"log"
 	"time"
-	ps "../MS_Generation/proto/"
-)
+
+	pb "github.com/iavealokin/microservices/MS_Generation/user"
+	"google.golang.org/grpc"
 )
 
+func main() {
+
+	//Открываем соединение, grpc.WithInsecure() означает,
+	//что шифрование не используется
+	conn, err := grpc.Dial("localhost:20100", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	/*
+
+	   Создаём нового клиента, используя соединение conn
+	   Обратим внимание на название клиента и на название сервиса,
+	   которое мы определили в proto-файле:
+
+	   service Mailer {
+	   rpc SendPass(MsgRequest) returns (MsgReply) {}
+	   rpc RetrievePass(MsgRequest) returns (MsgReply) {}
+	   }
+
+	*/
+
+	c := pb.NewMailerClient(conn)
+
+	//Определяем контекст с таймаутом в 1 секунду
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	/*
+	   Шлём запрос 1, ожидаем получение true в структуру rply
+	   типа MsgReply, определённую в прото-файле как:
+
+	   message MsgReply {
+	   bool sent = 1;
+	   }
+
+	*/
+
+	rply, err := c.SendPass(ctx, &pb.MsgRequest{"first", "test"})
+	if err != nil {
+		log.Println("something went wrong", err)
+	}
+	log.Println(rply.Sent)
+
+	//Шлём запрос 2, ожидаем false
+	rply, err = c.RetrievePass(ctx, &pb.MsgRequest{"second", "test"})
+	if err != nil {
+		log.Println("something went wrong", err)
+	}
+	log.Println(rply.Sent)
+
+}
+
+/*
 type User struct {
 	Login    string `json:"login"`
 	Username string `json:"username"`
@@ -145,3 +199,4 @@ func generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase int) 
 	})
 	return string(inRune)
 }
+*/
--- MS_Presistence/main.go
+++ MS_Presistence/main.go
@@ -1,33 +1,79 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
-	ps "MS_Generation/UserService/proto/"
-
+	pb "github.com/iavealokin/microservices/MS_Generation/user"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type User struct {
-	Login    string `json:"login"`
-	Username string `json:"username"`
-	Surname  string `json:"surname"`
-	Birthday string `json:"birthday"`
-	Password string `json:"password"`
+	Login    string
+	Username string
+	Surname  string
+	Birthday string
+	Password string
+}
+
+//Структура нашего gRPC сервера
+type server struct {
+}
+
+const port = ":20100"
+
+/*
+
+Методы структуры SendPass и RetrievePass принимают контекст и входящее сообщение,
+формат которого мы описали в proto-файле вот так:
+
+message MsgRequest {
+    string to = 1;
+    string code = 2;
+}
+
+Формат ответного сообщения в прото-файле был такой:
+
+message MsgReply {
+    bool sent = 1;
+}
+
+*/
+func (s *server) SendPass(ctx context.Context, in *pb.MsgRequest) (*pb.MsgReply, error) {
+
+	//Код писать не будем, просто ответим true на запрос
+
+	return &pb.MsgReply{Sent: true}, nil
+}
+
+func (s *server) RetrievePass(ctx context.Context, in *pb.MsgRequest) (*pb.MsgReply, error) {
+
+	//А здесь ответим false
+
+	return &pb.MsgReply{Sent: false}, nil
 }
 
 func main() {
-	server := grpc.NewServer()
-	instance := new(User)
-	ps.RegisterPasswordGeneratorServiceServer(server, instance)
 
-	listener, err := net.Listen("tcp", ":8080")
+	//Указываем на каком порту будем слушать запросы
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatal("Unable to create grpc listener:", err)
+		log.Fatal("failed to listen", err)
 	}
+	log.Printf("start listening for emails at port %s", port)
 
-	if err = server.Serve(listener); err != nil {
-		log.Fatal("Unable to start server:", err)
+	//Создаём новый grpc сервер
+	rpcserv := grpc.NewServer()
+
+	//Регистрируем связку сервер + listener
+	pb.RegisterUserServer(rpcserv, &server{})
+	reflection.Register(rpcserv)
+
+	//Запускаемся и ждём RPC-запросы
+	err = rpcserv.Serve(listener)
+	if err != nil {
+		log.Fatal("failed to serve", err)
 	}
 }
